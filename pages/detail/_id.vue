3e789483aa8c2f690a547866a9749e23ccb1071c
wurh <ro87630872@126.com>
2017-12-27 09:18:06 +0800

    [ADD]
    增加详情页部分

---
+++ pages/detail/_id.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="container">
+        Detail 详情页
+  </section>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+           
+        }
+    },
+    methods:{
+     
+    }
+}
+</script>
+
+<style>
+
+</style>

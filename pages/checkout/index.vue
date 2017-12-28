321ac594845e27d64e4a05abeccfbfdc2018610f
wurh <ro87630872@126.com>
2017-12-28 10:34:48 +0800

    UPD
    update code

--- pages/checkout/index.vue
+++ pages/checkout/index.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="container">
-        <mt-header fixed title="支付确认页">
+        <mt-header fixed title="支付确认页111">
            <div @click="onHistory"  slot="left">
                 <mt-button icon="back">back</mt-button>
             </div>
@@ -36,6 +36,14 @@ export default {
       totalPrice: 0
     };
   },
+  computed: {
+    carts() {
+      return this.$store.state.cart.list;
+    }
+  },
+  created() {
+    console.log(this.carts);
+  },
   methods: {
     onHistory() {
       window.history.go(-1);
@@ -58,78 +66,78 @@ export default {
 </script>
 
 <style scoped>
-.product-list{
-    margin-top: 60px;
+.product-list {
+  margin-top: 60px;
 }
 .shipping-wrap .product-list {
-    border-top: 1px solid #E4E4E4;
-    border-bottom: 1px solid #E4E4E4;
-    padding: .75rem .75rem 0;
+  border-top: 1px solid #e4e4e4;
+  border-bottom: 1px solid #e4e4e4;
+  padding: 0.75rem 0.75rem 0;
 }
 .shipping-wrap .product-list .product {
-    position: relative;
-    color: #666;
-    margin-bottom: .75rem;
-    min-height: 4.38rem;
+  position: relative;
+  color: #666;
+  margin-bottom: 0.75rem;
+  min-height: 4.38rem;
 }
 .shipping-wrap .product-list .product .inner-right {
-    margin-left: 4.5rem;
-    padding-left: 0;
+  margin-left: 4.5rem;
+  padding-left: 0;
 }
-.shipping-wrap .product-list .product .inner-right>* {
-    margin-bottom: .5rem;
+.shipping-wrap .product-list .product .inner-right > * {
+  margin-bottom: 0.5rem;
 }
 .shipping-wrap .product-list .product .title {
-    overflow: hidden;
-    text-overflow: ellipsis;
-    white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
 }
-.shipping-wrap .product-list .product .inner-right>* {
-    margin-bottom: .5rem;
+.shipping-wrap .product-list .product .inner-right > * {
+  margin-bottom: 0.5rem;
 }
 .shipping-wrap .product-list .product .amount {
-    float: right;
+  float: right;
 }
 .shipping-wrap .press-color {
-    color: #333;
-    font-weight: 700;
+  color: #333;
+  font-weight: 700;
 }
-.shipping-wrap .product-list .product .inner-right>* {
-    margin-bottom: .5rem;
+.shipping-wrap .product-list .product .inner-right > * {
+  margin-bottom: 0.5rem;
 }
 .shipping-wrap .press-color {
-    color: #333;
-    font-weight: 700;
+  color: #333;
+  font-weight: 700;
 }
 .shipping-wrap .product-list .product .money .default {
-    text-decoration: line-through;
-    font-size: .75rem;
+  text-decoration: line-through;
+  font-size: 0.75rem;
 }
 .shipping-wrap .product-list .product .product-img {
-    position: absolute;
-    background-position: center;
-    background-size: cover;
-    left: 0;
-    top: 0;
-    width: 3.44rem;
-    height: 4.38rem;
+  position: absolute;
+  background-position: center;
+  background-size: cover;
+  left: 0;
+  top: 0;
+  width: 3.44rem;
+  height: 4.38rem;
 }
 .shipping-wrap .product-list .product .product-img {
-    position: absolute;
-    background-position: center;
-    background-size: cover;
-    left: 0;
-    top: 0;
-    width: 3.44rem;
-    height: 4.38rem;
+  position: absolute;
+  background-position: center;
+  background-size: cover;
+  left: 0;
+  top: 0;
+  width: 3.44rem;
+  height: 4.38rem;
 }
-.product-img img{
-    position: absolute;
-    background-position: center;
-    background-size: cover;
-    left: 10px;
-    top: 60px;
-    width: 3.44rem;
-    height: 4.38rem;
+.product-img img {
+  position: absolute;
+  background-position: center;
+  background-size: cover;
+  left: 10px;
+  top: 60px;
+  width: 3.44rem;
+  height: 4.38rem;
 }
 </style>

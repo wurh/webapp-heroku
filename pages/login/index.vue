2e52634eeb6b33fc9c14b35b88e793e3164b3ea1
wurh <ro87630872@126.com>
2017-12-28 15:41:59 +0800

    [ADD]
    新增登录页

---
+++ pages/login/index.vue
@@ -0,0 +1,41 @@
+<template>
+   <section class="container">
+        <mt-header fixed title="login Page">
+        </mt-header>
+        <div class="login-form">
+            <mt-field label="username" placeholder="请输入用户名" v-model="username"></mt-field>
+            <mt-field label="password" placeholder="请输入密码" type="password" v-model="password"></mt-field>
+            <mt-field label="email" placeholder="请输入邮箱" type="email" v-model="email"></mt-field>
+        </div>
+        <div class="login-btn">
+             <mt-button type="primary" size="large" >Login</mt-button>
+        </div>
+    </section>
+</template>
+
+<script>
+import { mapMutations } from "vuex";
+export default {
+  data() {
+    return {
+        username:'',
+        password:'',
+        email:''
+    };
+  },
+  methods: {
+    
+  }
+};
+</script>
+
+<style scoped>
+    .login-form{
+        margin-top: 60px;
+    }
+    .login-btn{
+        text-align: center;
+        margin-top: 20px;
+        padding: 1rem;
+    }
+</style>
---
+++ pages/register/index.vue
@@ -0,0 +1,24 @@
+<template>
+   <section class="container">
+        <mt-header fixed title="Register Page">
+        </mt-header>
+  
+    </section>
+</template>
+
+<script>
+import { mapMutations } from "vuex";
+export default {
+  data() {
+    return {
+    };
+  },
+  methods: {
+    
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
